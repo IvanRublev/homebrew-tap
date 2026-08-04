f7e7dd1b40bd13d27ce5dfe70111f256e55fc3e4
Ivan Rublev <IvanRublev@users.noreply.github.com>
2026-08-04 23:02:24 +0000

    claudezero 0.0.17

--- claudezero.rb
+++ claudezero.rb
@@ -1,8 +1,8 @@
 class Claudezero < Formula
   desc "Multiple Claude Code(s) implement, commit and check off a software spec todos"
   homepage "https://github.com/IvanRublev/claudezero"
-  url "https://github.com/IvanRublev/claudezero/archive/refs/tags/v0.0.16.tar.gz"
-  sha256 "4a49c86c7e4a24813780d9c7ea67ae6c500e073363750fb86759e96e88166270"
+  url "https://github.com/IvanRublev/claudezero/archive/refs/tags/v0.0.17.tar.gz"
+  sha256 "0ea8ebfa9c348877bd838139e1ba2f1b2a82fef8d041c04ee2354d8348d55d10"
   license "MIT"
   head "https://github.com/IvanRublev/claudezero", branch: "master"
 
